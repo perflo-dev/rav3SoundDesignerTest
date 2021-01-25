1751355a32295655266764853e9d4f878fe33635
AlexanderIvanov <mail2a.v.ivanov@gmail.com>
2021-01-25 16:16:46 +0300

    done

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PARTY = 3177314091U;
+        static const AkUniqueID PLAY_BABY = 1308864102U;
+        static const AkUniqueID PLAY_DOGBARK = 1754141786U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_LAMP = 4234949920U;
+        static const AkUniqueID PLAY_ROOFWIND = 441471302U;
+        static const AkUniqueID PLAY_ROOMTONE = 2748311587U;
+        static const AkUniqueID PLAY_STREET = 2779137687U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AZIMUTH = 1437246667U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SB = 1584861542U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ Assets/footstep_script.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footstep_script : MonoBehaviour
+    {
+        public AK.Wwise.Event footstep;
+        public GameObject foot;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown("w"))
+            {
+                footstep.Post(foot);                
+            }
+        if (Input.GetKeyUp("w"))
+        {
+            footstep.Stop(foot);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            footstep.Post(foot);
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            footstep.Stop(foot);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            footstep.Post(foot);
+        }
+        if (Input.GetKeyUp("s"))
+        {
+            footstep.Stop(foot);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            footstep.Post(foot);
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            footstep.Stop(foot);
+        }
+    }
+    }
